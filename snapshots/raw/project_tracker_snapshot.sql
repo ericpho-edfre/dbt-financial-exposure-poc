f32fdbe672fc737a7a0c48a381390a4b13afecaf
ericpho-edfre <134000464+ericpho-edfre@users.noreply.github.com>
2025-07-08 18:26:29 -0700

    Updated snapshot by monitoring list of fields

--- snapshots/raw/project_tracker_snapshot.sql
+++ snapshots/raw/project_tracker_snapshot.sql
@@ -2,11 +2,35 @@
 
 {{
   config(
-    unique_key='project_id',
+    unique_key='PROJECT_ID',
     strategy='check',
-    check_cols='all'
+    check_cols=[
+      'PROJECT_NAME',
+      'LATITUDE',
+      'LONGITUDE',
+      'IS_PROJECT_EDF_OWNED',
+      'DEVELOPMENT_REGION',
+      'CURRENT_PROJECT_STATUS',
+      'TECHNOLOGY_CATEGORY',
+      'CONTRACTEDCAPACITYMWDC',
+      'CONTRACTEDCAPACITYMWAC',
+      'POICAPACITYMWAC',
+      'COD_DATE',
+      'EARLIEST_COD_DATE'
+      ]
   )
 }}
+
+{# List of unknown columns : 
+      -- 'PROJECT_STAGE' - Not found
+      -- 'PROJECT_TIER' - Not found
+      -- 'Stage Gate' - Not found
+      -- 'Project Tier' - Not found
+      -- 'BESSCapacity',
+      -- 'BESSEnergy',
+      -- 'LatestCOD' - Not found
+      -- 'FID' - Not found
+#}
 
 select *
 from {{ source('raw', 'project_tracker') }}
