06af932844cd9ad025342834365d8e30fb3bbbc0
Eric Pho <eric.pho@edf-re.com>
2025-07-31 05:30:54 +0000

    Trying to fix the macro

--- macros/overrides/generate_schema_name.sql
+++ macros/overrides/generate_schema_name.sql
@@ -1,12 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {% set env_name = env_var('DBT_ENV_NAME', 'DEV') | lower %}
-    {% set dev_user = env_var('DBT_DEV_USER', '') | lower %}
+    {% set env = env_var('DBT_ENV_NAME', 'dev') | lower %}
+    {% set user = env_var('DBT_DEV_USER', '') | lower %}
 
-    {% if env_name not in ['PROD', 'QUAL'] %}
-        {% if dev_user in ['', 'default'] %}
-            {{ exceptions.raise_compiler_error("Missing required environment variable: DBT_DEV_USER. Each developer must set this in their dbt Cloud profile.") }}
+    {% if env == 'dev' %}
+        {% if user in ['', 'default'] %}
+            {{ exceptions.raise_compiler_error("Missing DBT_DEV_USER in DEV environment. Please set it in your developer credentials.") }}
         {% endif %}
-        {{ 'DEV_' ~ dev_user ~ '_' ~ custom_schema_name }}
+        {{ 'DEV_' ~ user ~ '_' ~ custom_schema_name }}
     {% else %}
         {{ custom_schema_name }}
     {% endif %}
