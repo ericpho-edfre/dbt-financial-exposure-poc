998952a1ef15c81bfae54a3dcb353e7cd20a5171
Eric Pho <eric.pho@edf-re.com>
2025-07-31 05:48:57 +0000

    Trying to fix macro in upper environments

--- macros/overrides/generate_schema_name.sql
+++ macros/overrides/generate_schema_name.sql
@@ -1,12 +1,17 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    {% set is_pr = env_var('DBT_CLOUD_PR_ID', 'none') != 'none' %}
     {% set env = env_var('DBT_ENV_NAME', 'dev') | lower %}
-    {% set user = env_var('DBT_DEV_USER', '') | lower %}
+    {% set dev_user = env_var('DBT_DEV_USER', '') | lower %}
 
-    {% if env == 'dev' %}
-        {% if user in ['', 'default'] %}
-            {{ exceptions.raise_compiler_error("Missing DBT_DEV_USER in DEV environment. Please set it in your developer credentials.") }}
+    {% if is_pr %}
+        {{ custom_schema_name }}  {# Let dbt Cloud set the dynamic schema like dbt_cloud_pr_xxx #}
+
+    {% elif env == 'dev' %}
+        {% if dev_user in ['', 'default'] %}
+            {{ exceptions.raise_compiler_error("Missing DBT_DEV_USER in DEV environment.") }}
         {% endif %}
-        {{ 'DEV_' ~ user ~ '_' ~ custom_schema_name }}
+        {{ 'DEV_' ~ dev_user ~ '_' ~ custom_schema_name }}
+
     {% else %}
         {{ custom_schema_name }}
     {% endif %}
