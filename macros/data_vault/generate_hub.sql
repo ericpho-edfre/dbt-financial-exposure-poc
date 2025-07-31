c5bf1f395f512a18869dd134a79bcd37e38f6481
Eric Pho <eric.pho@edf-re.com>
2025-07-31 22:23:55 +0000

    Fixed hub creation by reinforcing using varchar for all business keys and introducing artificial UNKNOWN values

--- macros/data_vault/generate_hub.sql
+++ macros/data_vault/generate_hub.sql
@@ -1,11 +1,16 @@
-{% macro generate_hub(source_relation, business_keys, source_name) %}
+{% macro generate_hub(source_relation, business_keys, source_name, add_unknown_record=True) %}
+
+{%- set unknown_list = [] -%}
+{%- for col in business_keys -%}
+    {%- set _ = unknown_list.append("'UNKNOWN'") -%}
+{%- endfor -%}
 
 with source as (
 
     select
         {{ dbt_utils.generate_surrogate_key(business_keys) }} as hub_hk,
         {% for col in business_keys %}
-        {{ col }},
+        cast({{ col }} as varchar) as {{ col }},
         {% endfor %}
         current_timestamp as load_date,
         '{{ source_name }}' as record_source
@@ -34,6 +39,29 @@ deduplicated as (
 
 )
 
+{% if add_unknown_record %}
+,
+unknown_record as (
+    select 
+        {{ dbt_utils.generate_surrogate_key(unknown_list) }} as hub_hk,
+        {% for col in business_keys %}
+        'UNKNOWN' as {{ col }},
+        {% endfor %}
+        current_timestamp as load_date,
+        'SYSTEM' as record_source
+)
+{% endif %}
+
 select * from deduplicated
+
+{% if add_unknown_record %}
+union all
+select * from unknown_record
+{% if is_incremental() %}
+where {{ dbt_utils.generate_surrogate_key(unknown_list) }} not in (
+    select hub_hk from {{ this }}
+)
+{% endif %}
+{% endif %}
 
 {% endmacro %}
