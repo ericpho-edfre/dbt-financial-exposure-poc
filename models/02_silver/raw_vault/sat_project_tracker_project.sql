71db586c46f98a547cc59627d3bccd1cd14f69cf
Eric Pho <eric.pho@edf-re.com>
2025-08-01 21:54:36 +0000

    Added link generator

---
+++ models/02_silver/raw_vault/sat_project_tracker_project.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = ['hub_hk', 'load_date']
+) }}
+
+{{ generate_satellite_incremental(
+    source_model='stg_project_tracker',
+    business_key='project_id',
+    attributes=[
+        'project_name',
+        'latitude',
+        'longitude',
+        'is_project_edf_owned',
+        'development_region',
+        'current_project_status',
+        'technology_category',
+        'contracted_capacity_mw_dc',
+        'contracted_capacity_mw_ac',
+        'poi_capacity_mw_ac',
+        'cod_date',
+        'earliest_cod_date'
+    ],
+    record_source='"seed"',
+    load_date_expr='current_timestamp',
+    key_alias='hub_hk',
+    hashdiff_alias='hashdiff'
+) }}
